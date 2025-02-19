857aceb2fcc0e02da340c6fe21d09ad1e03ba48c
RandyNoodles <josh.rice193@gmail.com>
2025-02-19 17:50:33 -0500

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package cmd
+
+func main() {
+
+	//Load config settings
+
+	//Make sure our logFile exists/can be written to
+
+	//Init abuse prevention system
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module LoggingService
+
+go 1.24.0
